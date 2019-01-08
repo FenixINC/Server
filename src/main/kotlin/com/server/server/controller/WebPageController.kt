db5bb0e8395060f34076c985edec7e6949eea088
Taras Koloshmatin <taras@pier2marketing.com>
2019-01-08 15:21:10 +0200

    implement page delete

--- src/main/kotlin/com/server/server/controller/WebPageController.kt
+++ src/main/kotlin/com/server/server/controller/WebPageController.kt
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
@@ -16,9 +17,14 @@ class WebPageController {
     @GetMapping("/pages")
     fun doPages(model: Model): String {
         model.addAttribute("pageList", mPageList)
+        val tmpPage1 = Page()
+        tmpPage1.id = 1
+        tmpPage1.title = "tmp_title_1"
+        mPageList.add(tmpPage1)
         return "pages"
     }
 
+    // Page Create:
     @GetMapping("/page/create")
     fun doPageCreate(model: Model): String {
         val page = Page()
@@ -36,4 +42,18 @@ class WebPageController {
         }
         return "/page/create"
     }
+
+    // Page Delete:
+    @GetMapping("/page/delete/{id}")
+    fun doPageDelete(model: Model, @PathVariable("id") id: Long): String {
+        //TODO: implement delete Page
+
+        return "redirect:/pages"
+    }
+
+//    @PostMapping("/page/delete/{id}")
+//    fun doPageDelete(@PathVariable("id") id: Long, @ModelAttribute("page") page: Page, model: Model): String {
+//        model.addAttribute("formPageDelete", page)
+//        return "redirect:/pages"
+//    }
 }
